b121f2230a9f969e2674e19e0069b1b44b1d8d2e
Tian Pan <puncsky@gmail.com>
2019-10-06 16:48:56 -0700

    add fullcontact api gateway

--- src/api-gateway/resolvers/meta-resolver.ts
+++ src/api-gateway/resolvers/meta-resolver.ts
@@ -1,8 +1,31 @@
-import { Query } from "type-graphql";
+import { Args, ArgsType, Ctx, Field, Query } from "type-graphql";
+import { Context } from "../api-gateway";
+
+@ArgsType()
+class FullContactReqeust {
+  @Field(_ => String, { nullable: true })
+  email: string;
+  @Field(_ => String, { nullable: true })
+  emailHash: string;
+  @Field(_ => String, { nullable: true })
+  twitter: string;
+  @Field(_ => String, { nullable: true })
+  phone: string;
+}
 
 export class MetaResolver {
   @Query(_ => String, { description: "is the server healthy?" })
   public async health(): Promise<string> {
     return "OK";
+  }
+
+  @Query(_ => String)
+  public async fullContact(
+    @Args() fullContactRequest: FullContactReqeust,
+    @Ctx() { gateways }: Context
+  ): Promise<string> {
+    return JSON.stringify(
+      await gateways.fullcontact.getPerson(fullContactRequest)
+    );
   }
 }
