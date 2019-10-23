5914f5c39918fc8ffdafdd6220e2adad9328461d
ykc <yuekec@gmail.com>
2019-10-23 23:24:25 +0800

    fix lint error

--- src/shared/contact/phone-input/util.ts
+++ src/shared/contact/phone-input/util.ts
@@ -6,20 +6,23 @@ interface CountryInfo {
   callingCode: string;
 }
 
-let callingCodeCountryMap: { [x: number]: CountryInfo };
+let callingCodeCountryMap: { [x: string]: CountryInfo };
 export function guessCountry(phone: string): CountryInfo {
   if (!callingCodeCountryMap) {
-    callingCodeCountryMap = countries.reduce((acc, country) => {
-      const callingCode = country[1] as string;
-      if (!acc[callingCode] || country[country.length - 1] === 1) {
-        acc[callingCode] = {
-          iso2: country[3],
-          callingCode: country[1],
-          format: typeof country[4] === "string" ? country[4] : undefined
-        };
-      }
-      return acc;
-    }, {});
+    callingCodeCountryMap = countries.reduce(
+      (acc, country) => {
+        const callingCode = country[1] as string;
+        if (!acc[callingCode] || country[country.length - 1] === 1) {
+          acc[callingCode] = {
+            iso2: country[3],
+            callingCode: country[1],
+            format: typeof country[4] === "string" ? country[4] : undefined
+          };
+        }
+        return acc;
+      },
+      {} as any
+    );
   }
   const text = phone.replace(/\D/g, "");
   for (let i = 0; i < 3 && i < text.length; i++) {
@@ -34,10 +37,13 @@ export function guessCountry(phone: string): CountryInfo {
 let iso2CallingCodeMap: { [x: string]: string };
 export function getCallingCode(iso2: string): string {
   if (!iso2CallingCodeMap) {
-    iso2CallingCodeMap = countries.reduce((acc, country) => {
-      acc[country[3]] = country[1];
-      return acc;
-    }, {});
+    iso2CallingCodeMap = countries.reduce(
+      (acc, country) => {
+        acc[country[3]] = country[1];
+        return acc;
+      },
+      {} as any
+    );
   }
   return iso2CallingCodeMap[iso2];
 }
