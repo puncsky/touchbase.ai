e1b1c75942e376aa58a3fd33b58cad090c2ce8b7
menq <mengqiang.q@gmail.com>
2019-11-05 00:18:36 -0500

    Update: update ui mutation

--- src/shared/settings/danger-zone.tsx
+++ src/shared/settings/danger-zone.tsx
@@ -2,14 +2,22 @@ import { Form } from "antd";
 import Button from "antd/lib/button/button";
 import Modal from "antd/lib/modal";
 import Typography from "antd/lib/typography";
+import gql from "graphql-tag";
 import { t } from "onefx/lib/iso-i18n";
 import React, { Component } from "react";
+import Mutation, { MutationFn } from "react-apollo/Mutation";
 import { Flex } from "../common/flex";
 import { FieldMargin } from "../onefx-auth-provider/email-password-identity-provider/view/field-margin";
 import { InputError } from "../onefx-auth-provider/email-password-identity-provider/view/input-error";
 import { InputLabel } from "../onefx-auth-provider/email-password-identity-provider/view/input-label";
 import { TextInput } from "../onefx-auth-provider/email-password-identity-provider/view/text-input";
 const { Text } = Typography;
+
+const DELETE_ACCOUNT = gql`
+  mutation deleteAccount($email: String!) {
+    deleteAccount(deleteAccountInput: { email: $email })
+  }
+`;
 
 type State = {
   modalShow: boolean;
@@ -29,7 +37,10 @@ export class DangerZone extends Component<{}, State> {
     });
   };
 
-  public handleOk = () => {
+  public handleOk = async (func: MutationFn<{ email: string }>) => {
+    await func({
+      variables: { email: "" }
+    });
     this.setState({
       modalShow: false
     });
@@ -57,43 +68,55 @@ export class DangerZone extends Component<{}, State> {
         <Button type="danger" onClick={this.showModal}>
           {t("settings.deleteAccount")}
         </Button>
-        <Modal
-          title={t("settings.modal_title")}
-          visible={this.state.modalShow}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <Text mark>{t("settings.modal_danger")}</Text>
-          <p>
-            <Text>{t("settings.modal_email_delete_desc")}</Text>
-          </p>
-          <Form id={"email-verify"}>
-            <FieldMargin>
-              <InputLabel>{t("settings.modal_email_input_label")}</InputLabel>
-              <TextInput
-                defaultValue={""}
-                error={""}
-                type="email"
-                aria-label="Your Email"
-                name="email"
-                placeholder={t("field.email")}
-              />
-              <InputError> </InputError>
-            </FieldMargin>
-            <FieldMargin>
-              <InputLabel>{t("settings.email_verify")}</InputLabel>
-              <TextInput
-                defaultValue=""
-                error=""
-                type="text"
-                aria-label="New Password"
-                name=""
-                placeholder=""
-              />
-              <InputError> </InputError>
-            </FieldMargin>
-          </Form>
-        </Modal>
+        <Mutation mutation={DELETE_ACCOUNT}>
+          {(
+            deleteContact: MutationFn<{ email: string }>,
+            { loading }: { loading: boolean }
+          ) => {
+            return (
+              <Modal
+                title={t("settings.modal_title")}
+                visible={this.state.modalShow}
+                confirmLoading={loading}
+                onOk={() => this.handleOk(deleteContact)}
+                onCancel={this.handleCancel}
+              >
+                <Text mark>{t("settings.modal_danger")}</Text>
+                <p>
+                  <Text>{t("settings.modal_email_delete_desc")}</Text>
+                </p>
+                <Form id={"email-verify"}>
+                  <FieldMargin>
+                    <InputLabel>
+                      {t("settings.modal_email_input_label")}
+                    </InputLabel>
+                    <TextInput
+                      defaultValue={""}
+                      error={""}
+                      type="email"
+                      aria-label="Your Email"
+                      name="email"
+                      placeholder={t("field.email")}
+                    />
+                    <InputError> </InputError>
+                  </FieldMargin>
+                  <FieldMargin>
+                    <InputLabel>{t("settings.email_verify")}</InputLabel>
+                    <TextInput
+                      defaultValue=""
+                      error=""
+                      type="text"
+                      aria-label="New Password"
+                      name=""
+                      placeholder=""
+                    />
+                    <InputError> </InputError>
+                  </FieldMargin>
+                </Form>
+              </Modal>
+            );
+          }}
+        </Mutation>
       </Flex>
     );
   }
