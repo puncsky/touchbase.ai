4c505a661ab619d05f0524e54fda145217e4e086
menq <mengqiang.q@gmail.com>
2019-11-06 20:49:46 -0500

    Fix: fix ts

--- src/api-gateway/resolvers/account-resolver.ts
+++ src/api-gateway/resolvers/account-resolver.ts
@@ -1,21 +1,13 @@
 import { AuthenticationError } from "apollo-server-errors";
-import { Arg, Ctx, Field, InputType, Mutation } from "type-graphql";
+import { Ctx, Mutation } from "type-graphql";
 
 import { Context } from "../api-gateway";
 
-@InputType()
-class DeleteAccountInput {
-  @Field(_ => String)
-  email: string;
-}
-
 export class AccountResolve {
   @Mutation(_ => Boolean)
-  public async deleteAccount(
-    // @ts-ignore
-    @Arg("deleteAccountInput") input: DeleteAccountInput,
-    @Ctx() { auth, userId, model }: Context
-  ): Promise<boolean> {
+  public async deleteAccount(@Ctx() { auth, userId, model }: Context): Promise<
+    boolean
+  > {
     if (!userId) {
       throw new AuthenticationError(`please login to deleteContactInput`);
     }
@@ -25,15 +17,17 @@ export class AccountResolve {
       throw new AuthenticationError(`user is not found`);
     }
 
-    if (user.email === input.email) {
+    try {
       const { contact, event, human, personalNote } = model;
-      await contact.deleteAllByOwner({ ownerId: userId });
-      await event.deleteAllByOwner({ ownerId: userId });
-      await human.deleteAllByOwner({ ownerId: userId });
-      await personalNote.deleteAllByOwner({ ownerId: userId });
-      await auth.user.deleteById(userId);
+      await Promise.all([
+        contact.deleteAllByOwner({ ownerId: userId }),
+        event.deleteAllByOwner({ ownerId: userId }),
+        human.deleteAllByOwner({ ownerId: userId }),
+        personalNote.deleteAllByOwner({ ownerId: userId }),
+        auth.user.deleteById(userId)
+      ]);
       return Boolean(true);
-    } else {
+    } catch (e) {
       return Boolean(false);
     }
   }
