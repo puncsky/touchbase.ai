f2cf6194366fbea38b038e7528a6d04d6f30f7fb
menq <mengqiang.q@gmail.com>
2019-11-17 00:25:23 -0500

    add: add tag-resolver

--- src/types/tag.ts
+++ src/types/tag.ts
@@ -1,16 +1,7 @@
-export interface TTagTemplate {
-  id?: string;
-  name: string;
-  ownerId: string;
-  hasRate: boolean;
-}
-
 export interface TTag {
-  id?: string;
-  rate: number;
-  templateId: string;
-  contactId: string;
+  _id?: string;
   name: string;
   ownerId: string;
-  hasRate: boolean;
+  rate?: number;
+  contactId: string;
 }
