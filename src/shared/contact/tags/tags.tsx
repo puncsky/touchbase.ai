a10d1eed42d4c8359f70c8dd3c0c58bac3cf2546
menq <mengqiang.q@gmail.com>
2019-12-12 23:42:55 -0500

    Update: update tag UI

--- src/shared/contact/tags/tags.tsx
+++ src/shared/contact/tags/tags.tsx
@@ -1,18 +1,15 @@
 import {
-  Checkbox,
+  AutoComplete,
   Dropdown,
   Form,
   Icon,
   Input,
   Menu,
-  Modal,
   Rate,
-  Tag,
-  AutoComplete
+  Tag
 } from "antd";
 import Divider from "antd/lib/divider";
 import { FormProps, WrappedFormUtils } from "antd/lib/form/Form";
-import notification from "antd/lib/notification";
 import gql from "graphql-tag";
 import { t } from "onefx/lib/iso-i18n";
 import { styled } from "onefx/lib/styletron-react";
@@ -22,7 +19,6 @@ import Mutation, { MutationFn } from "react-apollo/Mutation";
 import { TTag, TTagTemplate } from "../../../types/tag";
 import { Flex } from "../../common/flex";
 import { Preloader } from "../../common/preloader";
-import { SelectContainer } from "./select";
 
 const Container = styled("div", {
   width: "100%",
@@ -32,8 +28,8 @@ const Container = styled("div", {
 type State = {
   modalShow: boolean;
   showInput: boolean;
-  inputValue: string;
   dataSource: Array<string>;
+  inputValue: string;
 };
 
 type Props = {
@@ -98,65 +94,86 @@ class Tags extends React.Component<Props, State> {
     };
   }
 
-  showModal = () => {
-    this.setState({
-      modalShow: true
-    });
-  };
-
-  createTag = (createTag: MutationFn, refetch: () => void) => {
-    const { form } = this.props;
-    const { resetFields } = form;
-    if (!form) {
-      return;
-    }
-    form.validateFields(async (err, values) => {
-      if (err) {
-        return;
-      }
-      try {
-        await createTag({
-          variables: {
-            createTagTemplateInput: {
-              ...values
-            }
-          }
-        });
-        this.hideModal();
-        notification.success({
-          message: t("tag.create_success")
-        });
-        resetFields();
-        refetch();
-      } catch (e) {
-        notification.error({
-          message: t("tag.create_error"),
-          description: e
-        });
-      }
-    });
-  };
-
-  hideModal = () => {
-    this.setState({
-      modalShow: false
-    });
-  };
-
   showInput = () => {
     this.setState({ showInput: true });
   };
-  onSelect = (createTag: MutationFn, value: any) => {};
 
-  onSearch = (
+  onSelect = async (
+    createTag: MutationFn,
+    value: string,
+    unselected: Array<TTagTemplate>,
+    refetch: any
+  ) => {
+    const template = unselected.find(item => item.name === value);
+    if (!template) {
+      return;
+    }
+    await createTag({
+      variables: {
+        createTagInput: {
+          templateId: template.id,
+          contactId: this.props.contactId,
+          rate: 5
+        }
+      }
+    });
+    await refetch();
+    this.onInputBlur();
+  };
+
+  onInputBlur = () => {
+    this.setState({
+      showInput: false,
+      inputValue: ""
+    });
+  };
+
+  onChange = (value: string, data: Array<TTagTemplate>) => {
+    this.setState({
+      inputValue: value
+    });
+    let dataSource: Array<string>;
+    if (value === "") {
+      dataSource = data.map(item => item.name);
+    } else {
+      dataSource = data
+        .filter(item =>
+          item.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+        )
+        .map(item => item.name);
+    }
+    this.setState({
+      dataSource: dataSource
+    });
+  };
+
+  createTemplateAndTag = async (
     createTemplateTag: MutationFn,
     createTag: MutationFn,
-    searchText: string
-  ) => {};
+    refetch: any
+  ) => {
+    const template = (await createTemplateTag({
+      variables: {
+        createTagTemplateInput: {
+          name: this.state.inputValue,
+          hasRate: true
+        }
+      }
+    })) as any;
 
-  onChange = (value: string) => {};
+    await createTag({
+      variables: {
+        createTagInput: {
+          templateId: template.data.createTagTemplate.id,
+          contactId: this.props.contactId,
+          rate: 5
+        }
+      }
+    });
 
-  saveInputRef = (input: Input) => (this.input = input);
+    await refetch();
+    this.onInputBlur();
+  };
 
   render(): JSX.Element {
     return (
@@ -176,17 +193,14 @@ class Tags extends React.Component<Props, State> {
             if (loading || !data) {
               return <TagSpin />;
             }
+            const unselectedTemplates = data.getUserTagTemplates.filter(
+              template =>
+                data.getContactTags.findIndex(
+                  tag => tag.templateId === template.id
+                ) === -1
+            );
             return (
               <>
-                <SelectContainer
-                  contactId={this.props.contactId}
-                  refetch={refetch}
-                  data={data}
-                  showModal={() => {
-                    this.showModal();
-                  }}
-                />
-                <Divider />
                 <Flex
                   flexDirection="row"
                   alignContent="flex-start"
@@ -202,47 +216,14 @@ class Tags extends React.Component<Props, State> {
                       </Menu>
                     );
                     return (
-                      <div key={item.id}>
+                      <div key={item.id} style={{ padding: "5px" }}>
                         <Dropdown overlay={menu}>
                           <Tag>{item.name}</Tag>
                         </Dropdown>
                       </div>
                     );
                   })}
-                  {this.state.showInput && (
-                    <Mutation mutation={CREATE_TAG}>
-                      {(createTag: MutationFn) => (
-                        <Mutation mutation={CREATE_TEMPLATE_TAG}>
-                          {(createTemplateTag: MutationFn) => (
-                            <AutoComplete
-                              autoFocus
-                              dataSource={this.state.dataSource}
-                              style={{ width: 200 }}
-                              onSelect={value =>
-                                this.onSelect(createTag, value)
-                              }
-                              onSearch={(value: string) =>
-                                this.onSearch(
-                                  createTemplateTag,
-                                  createTag,
-                                  value
-                                )
-                              }
-                              placeholder="input here"
-                            />
-                          )}
-                        </Mutation>
-                      )}
-                    </Mutation>
-                  )}
-                  {!this.state.showInput && (
-                    <Tag
-                      onClick={this.showInput}
-                      style={{ background: "#fff", borderStyle: "dashed" }}
-                    >
-                      <Icon type="plus" /> New Tag
-                    </Tag>
-                  )}
+                  {this.renderInput(unselectedTemplates, refetch)}
                 </Flex>
                 <Divider />
               </>
@@ -252,6 +233,71 @@ class Tags extends React.Component<Props, State> {
       </Container>
     );
   }
+
+  renderInput = (unselectedTemplates: Array<TTagTemplate>, refetch: any) => {
+    const { inputValue, dataSource } = this.state;
+    return (
+      <>
+        {this.state.showInput && (
+          <Mutation mutation={CREATE_TAG}>
+            {(createTag: MutationFn) => (
+              <Mutation mutation={CREATE_TEMPLATE_TAG}>
+                {(createTemplateTag: MutationFn) => (
+                  <AutoComplete
+                    value={inputValue}
+                    autoFocus
+                    defaultOpen
+                    dataSource={
+                      inputValue !== ""
+                        ? dataSource
+                        : unselectedTemplates.map(item => item.name)
+                    }
+                    style={{ width: 150 }}
+                    onSelect={value => {
+                      this.onSelect(
+                        createTag,
+                        value as string,
+                        unselectedTemplates,
+                        refetch
+                      );
+                    }}
+                    placeholder={t("tag.create_tag")}
+                    onBlur={this.onInputBlur}
+                    onChange={value => {
+                      this.onChange(value as string, unselectedTemplates);
+                    }}
+                  >
+                    <Input
+                      onPressEnter={() => {
+                        this.createTemplateAndTag(
+                          createTemplateTag,
+                          createTag,
+                          refetch
+                        );
+                      }}
+                    />
+                  </AutoComplete>
+                )}
+              </Mutation>
+            )}
+          </Mutation>
+        )}
+        {!this.state.showInput && (
+          <Tag
+            onClick={this.showInput}
+            style={{
+              background: "#fff",
+              borderStyle: "dashed",
+              padding: "5px"
+            }}
+          >
+            <Icon type="plus" />
+            {t("tag.add_tag")}
+          </Tag>
+        )}
+      </>
+    );
+  };
 }
 
 export const TagsContainer = Form.create<Props & { form: WrappedFormUtils }>({
