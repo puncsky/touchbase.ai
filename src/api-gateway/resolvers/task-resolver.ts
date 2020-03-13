efda32442eac16c52d0999580283afc4af0cc06a
menq <mengqiang.q@gmail.com>
2020-03-12 23:05:13 -0400

    add: add task curd

---
+++ src/api-gateway/resolvers/task-resolver.ts
@@ -0,0 +1,119 @@
+import { AuthenticationError } from "apollo-server-errors";
+import {
+  Arg,
+  Args,
+  ArgsType,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query
+} from "type-graphql";
+import { TTask } from "../../types/task";
+import { Context } from "../api-gateway";
+
+@InputType()
+class CreateTaskInput {
+  @Field(_ => String)
+  public title: string;
+  @Field(_ => Boolean)
+  public done: boolean;
+  @Field(_ => String)
+  public rrule: string;
+  @Field(_ => Date)
+  public due: Date;
+  @Field(_ => [String])
+  public contacts: Array<string>;
+}
+
+@ArgsType()
+class DeleteTaskInput {
+  @Field(_ => String)
+  public id: string;
+}
+
+@InputType()
+class UpsertTaskInput {
+  @Field(_ => String)
+  public id: string;
+  @Field(_ => String)
+  public title: string;
+  @Field(_ => Boolean)
+  public done: boolean;
+  @Field(_ => String)
+  public rrule: string;
+  @Field(_ => Date)
+  public due: Date;
+  @Field(_ => [String])
+  public contacts: Array<string>;
+}
+
+@ObjectType()
+class Task {
+  @Field(_ => String)
+  public id: string;
+  @Field(_ => String)
+  public title: string;
+  @Field(_ => Boolean)
+  public done: boolean;
+  @Field(_ => [String])
+  public contacts: Array<string>;
+  @Field(_ => String)
+  public rrule: string;
+  @Field(_ => Date)
+  public due: Date;
+  @Field(_ => String)
+  public ownerId: string;
+}
+
+export class TaskResolver {
+  @Query(_ => [Task])
+  public async getUserTasks(
+    @Ctx() { model, userId }: Context
+  ): Promise<Array<TTask>> {
+    if (!userId) {
+      throw new AuthenticationError(`please login to getUserTasks`);
+    }
+    return model.task.getTaskByUser(userId);
+  }
+
+  @Mutation(_ => Boolean)
+  public async deleteTask(
+    @Args() { id }: DeleteTaskInput,
+    @Ctx() { model, userId }: Context
+  ): Promise<Boolean> {
+    if (!userId) {
+      throw new AuthenticationError(`please login to deleteTask`);
+    }
+    return model.task.deleteTask(id, userId);
+  }
+
+  @Mutation(_ => Task)
+  public async createTask(
+    @Arg("createTaskInput") createTaskInput: CreateTaskInput,
+    @Ctx() { model, userId }: Context
+  ): Promise<TTask> {
+    if (!userId) {
+      throw new AuthenticationError(`please login to createTask`);
+    }
+    return model.task.createTask({
+      ...createTaskInput,
+      ownerId: userId
+    });
+  }
+
+  @Mutation(_ => Task)
+  public async upsertTask(
+    @Arg("upsertTaskInput") upsertTaskInput: UpsertTaskInput,
+    @Ctx() { model, userId }: Context
+  ): Promise<TTask | null> {
+    if (!userId) {
+      throw new AuthenticationError(`please login to upsertTask`);
+    }
+    return model.task.upsert({
+      ...upsertTaskInput,
+      ownerId: userId
+    });
+  }
+}
